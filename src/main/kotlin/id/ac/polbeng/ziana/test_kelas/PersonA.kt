07f230fcab2a893b6d631c9aec2a0f3cb615a871
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-24 11:09:36 +0700

    Kode Program Ketiga yaitu Penggunaan Initializer Block untuk menginisialisasi property.

---
+++ src/main/kotlin/id/ac/polbeng/ziana/test_kelas/PersonA.kt
@@ -0,0 +1,21 @@
+package id.ac.polbeng.ziana.test_kelas
+
+class PersonA (_firstName: String, _lastName: String, _age:Int){
+    var firstName: String
+    var lastName: String
+    var age: Int
+    init {
+        firstName = _firstName
+        lastName = _lastName
+        age = _age
+    }
+}
+/*
+Parameter dari konstruktor utama dapat digunakan dalam blok inisialisasi serta
+dapat digunakan untuk menginisialisasi property yang di deklarasikan di dalam tubuh kelas.
+ */
+fun main() {
+    val ziana = PersonA("Ziana","Aulia", 19)
+    println("Name : ${ziana.firstName} ${ziana.lastName}")
+    println("Age: ${ziana.age}")
+}
