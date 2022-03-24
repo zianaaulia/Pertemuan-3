a581304a9311cc07d112b2bc9e7ac90ae92da2be
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-24 11:48:32 +0700

    Kode Program Ketujuh yaitu Penggunaan Secondary Constructor yang didefinisikan lebih dari satu.

---
+++ src/main/kotlin/id/ac/polbeng/ziana/test_kelas/PersonE.kt
@@ -0,0 +1,29 @@
+package id.ac.polbeng.ziana.test_kelas
+
+class PersonE {
+    var firstName: String
+    var lastName: String
+    var age: Int = 20
+    constructor(_firstName: String, _lastName: String){
+        firstName = _firstName
+        lastName = _lastName
+    }
+    constructor(_firstName: String, _lastName: String, _age: Int): this(_firstName, _lastName){
+        age = _age
+    }
+    fun cetakInfo(){
+        println("Name : ${firstName} ${lastName}")
+        println("Age : ${age}")
+    }
+}
+
+fun main() {
+    val yuni = PersonE("Yuni", "Zanika")
+    yuni.cetakInfo()
+    println()
+    val nurul = PersonE("Nurul", "Hakiki")
+    nurul.cetakInfo()
+}
+/*
+Keyword this digunakan untuk merujuk pada konstruktor kelas yang sesuai.
+ */
