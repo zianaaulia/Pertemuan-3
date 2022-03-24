34e5f1483b150dc2689c92b9272509364e94b4e8
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-24 11:28:24 +0700

    Kode Program Kelima yaitu Penggunaan Initializer Block untuk meringkas kode program kelas Person dengan menggabungkan proses deklarasi dan inisialisasi hanya di dalam parameter konstruktor utama.

---
+++ src/main/kotlin/id/ac/polbeng/ziana/test_kelas/PersonC.kt
@@ -0,0 +1,12 @@
+package id.ac.polbeng.ziana.test_kelas
+/*
+Untuk lebih ringkas, deklarasi dan inisialisasi propert dapat dilakukan
+hanya di dalam parameter konstruktor utama.
+ */
+class PersonC(var firstName: String, var lastName: String, var age: Int) {}
+
+fun main() {
+    val ziana = PersonC("Ziana", "Aulias", 19)
+    println("Name : ${ziana.firstName} ${ziana.lastName}")
+    println("Age : ${ziana.age}")
+}
