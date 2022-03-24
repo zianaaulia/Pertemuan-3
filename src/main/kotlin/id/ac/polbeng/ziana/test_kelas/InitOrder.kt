5e53b7b2cc916f1f4cd6a93668ac5b9a5ca15c55
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-24 11:02:32 +0700

    Kode Program Kedua yaitu Penggunaan Initializer Block.

---
+++ src/main/kotlin/id/ac/polbeng/ziana/test_kelas/InitOrder.kt
@@ -0,0 +1,22 @@
+package id.ac.polbeng.ziana.test_kelas
+
+/*
+Blok inisialisasi diawali dengan keyword init.
+Kode di dalam blok init adalah yang pertama dieksekusi ketika kelas dipakai.
+Blok init dijalankan setiap kali kelas dipakai,
+dengan segala jenis konstruktor seperti yang akan kita lihat selanjutnya.
+ */
+class InitOrder(name: String) {
+    val firstProperty = "First property: $name".also(::println)
+    init {
+        println("First initializer block that prints ${name}")
+    }
+    val secondProperty = "Second property: ${name.length}".also(::println)
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+
+}
+fun main() {
+    val initOrder = InitOrder("RPL")
+}
