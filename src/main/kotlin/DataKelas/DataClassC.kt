2557f13d7951aab5a388f5b14f14bb69f956fa05
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-31 10:24:46 +0700

    Kode Program Ketiga Pada Data Kelas.

---
+++ src/main/kotlin/DataKelas/DataClassC.kt
@@ -0,0 +1,16 @@
+package DataKelas
+
+fun main(){
+    val meri = Student("Meri", 20)
+    val badu = meri.copy("Badu")
+    if(meri.equals(badu))
+        println("meri is equal to badu.")
+    else
+        println("meri is not equal to badu.")
+    println("Hashcode of meri: ${meri.hashCode()}")
+    println("Hashcode of badu: ${badu.hashCode()}")
+}
+/*
+Untuk sebuah data kelas, kita dapat membuat sebuah objek salinan(copy) dengan beberapa
+property yang berbeda menggunakan fungsi copy().
+ */
