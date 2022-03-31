8216623942e0e40d7d2bd5ec22428d48828ad363
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-31 10:26:23 +0700

    Kode Program Keempat Pada Data Kelas.

---
+++ src/main/kotlin/DataKelas/DataClassD.kt
@@ -0,0 +1,10 @@
+package DataKelas
+//Data class toString() method
+fun main(){
+    val meri = Student("Meri", 20)
+    println(meri.toString())
+}
+/*
+Fungsi toString() pada data kelas mengembalikan nilai kembalian String yang merupakan
+representasi dari objek.
+ */
